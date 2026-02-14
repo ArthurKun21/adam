55b9cda807aa082e2851bfc2acb492608db551d4
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2026-02-14 07:57:57 +0000

    chore: add opt-in compiler arguments for coroutines

--- adam/build.gradle.kts
+++ adam/build.gradle.kts
@@ -150,6 +150,10 @@ java {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>{
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        freeCompilerArgs.addAll(
+            "-opt-in=kotlinx.coroutines.DelicateCoroutinesApi",
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+        )
     }
 }
 
