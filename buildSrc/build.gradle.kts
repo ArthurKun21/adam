fee1cebda7b3ec390676d87ccbe4835000146bf7
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2026-02-14 07:50:24 +0000

    chore: setup spotless

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation(libs.kotlin.gradle)
     implementation(libs.androidx.gradle)
     implementation(libs.dokka.gradle)
+    implementation(libs.spotless.gradle)
 
     // workaround to enable version catalogs (libs) in buildSrc
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
