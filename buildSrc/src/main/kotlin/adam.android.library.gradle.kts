7d70bdb335fa6a30c9fa86b7055a00bdbe525d5c
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2026-02-14 08:26:04 +0000

    refactor: replace hardcoded jvmTarget with AndroidConfig value

--- buildSrc/src/main/kotlin/adam.android.library.gradle.kts
+++ buildSrc/src/main/kotlin/adam.android.library.gradle.kts
@@ -21,7 +21,7 @@ java {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>{
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        jvmTarget.set(AndroidConfig.JvmTarget)
     }
 }
 
