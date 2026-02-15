f7f64faf895a43f4020daaf39f3237132b531a12
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2026-02-15 04:43:56 +0000

    chore(deps): remove gradle-versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,13 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.gradle.api.publish.PublishingExtension
 import java.util.Locale
 
 plugins {
-    alias(libs.plugins.gradle.versions)
     alias(libs.plugins.vanniktech.maven.publish) apply false
 }
 
 version = providers.environmentVariable("RELEASE_TAG")
     .map { it.removePrefix("v") }
     .getOrElse(libs.versions.adam.get())
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.ENGLISH).contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
-}
 
 subprojects {
     version = rootProject.version
