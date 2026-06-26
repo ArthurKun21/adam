57ba9290370cd13ab42dfd22e2e1a75f8f48f5e8
Arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-06-26 23:08:11 +0800

    fix: missing logcat initialization

--- samples/desktopApp/src/main/kotlin/com/arthurkun21/adam/samples/desktop/Main.kt
+++ samples/desktopApp/src/main/kotlin/com/arthurkun21/adam/samples/desktop/Main.kt
@@ -4,8 +4,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.arthurkun21.adam.samples.desktop.ui.MainScreen
+import logcat.LogcatLogger
+import logcat.PrintLogger
 
 fun main() {
+    if (!LogcatLogger.isInstalled) {
+        LogcatLogger.install()
+        LogcatLogger.loggers += PrintLogger
+    }
+
     application {
         Window(
             onCloseRequest = ::exitApplication,
